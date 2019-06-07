c039e90728c3d8cafd2eea6b3dcaceb85e1d3a8b
Deirdre Connolly <durumcrustulum@gmail.com>
2019-06-06 23:02:52 -0400

    Remove some debug stuff, testing if running example5 alone matters

--- test-data/src/block.rs
+++ test-data/src/block.rs
@@ -605,7 +605,6 @@ fn example5() {
 
 	assert_eq!(hash, "f50c5629c1b6921cb9219574152b253dc72f7de96b0813aab75e2f2fb43e05e5".into());
 	assert_eq!(block.header().previous_header_hash, "0000000000000000000000000000000000000000000000000000000000000000".into());
-	assert_eq!(1, 0);
 }
 
 #[test]
